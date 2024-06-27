2cdbdd1fad03781d4940a19846ed387f0edb8744
yeochaeeon <codjs2659@pusan.ac.kr>
2024-06-27 19:40:59 +0900

    Update randomDice Generator

---
+++ 04_js/js_randomDice/randomDice.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("주사위 게임 입장 ");
+  const img = document.querySelector("img");
+  const bt = document.querySelector("button");
+  const images = [
+    "./img/1.png",
+    "./img/2.png",
+    "./img/3.png",
+    "./img/4.png",
+    "./img/5.png",
+    "./img/6.png",
+  ];
+
+  bt.addEventListener("click", () => {
+    const randomIndex = Math.floor(Math.random() * 6);
+    img.src = images[randomIndex];
+  });
+});
